536ccd3ec4a5283885a56c6959fcd12c58d75558
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-23 14:28:46 +0600

    Navbar updated

--- frontend-college-booking-app/src/components/Header/Navbar.jsx
+++ frontend-college-booking-app/src/components/Header/Navbar.jsx
@@ -1,31 +1,39 @@
 import { AppBar, Autocomplete, Box, Tab, Tabs, TextField, Toolbar } from "@mui/material"
 
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import { useState } from "react";
 
 
 const Navbar = () => {
 
 const dummyArray = ["kings college", "MotherLand", "FatherLand"]
+const [value, setValue] = useState(0);
 
 
 
   return (
-    <AppBar>
+    <AppBar  sx={{ bgcolor: "#2b2d42" }}>
         <Toolbar>
             <Box display="flex" width={"20%"}>
 <AccessAlarmIcon /> 
 <p >Campasian</p>
             </Box>
-            <Box width={"50%"} margin= {"auto"}>
+            <Box width={"20%"} margin= {"auto"}>
             <Autocomplete
+            sx={{ borderRadius: 10, width: "40%", margin: "auto" }}
         id="free-solo-demo"
         freeSolo
         options={dummyArray.map((option) => option)}
-        renderInput={(params) => <TextField variant="standard" {...params} label="Search College" />}
+        renderInput={(params) => <TextField sx={{
+            borderRadius: 2,
+            input: { color: "white" },
+            bgcolor: "#2b2d42",
+            padding: "6px",
+          }} variant="standard" {...params} label="Search College" />}
       />
             </Box>
             <Box display={"flex"}>
-                <Tabs textColor="white" indicatorColor="secondary" >
+                <Tabs textColor="white" indicatorColor="secondary" value={value} onChange={(e, val) => setValue(val)} >
                     <Tab label="All Colleges"/>
                     <Tab label="Auth"/>
                     <Tab label="Admin"/>
