4280982d5ce20f14ecb51078f63eb3b661c12be6
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-23 23:29:21 +0600

    User and admin Logout Function added

--- frontend-college-booking-app/src/components/Header/Navbar.jsx
+++ frontend-college-booking-app/src/components/Header/Navbar.jsx
@@ -1,14 +1,22 @@
 import { AppBar, Autocomplete, Box, Tab, Tabs, TextField, Toolbar, Typography } from "@mui/material"
-
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import { useEffect, useState } from "react";
 import { getAllColleges } from "../../api-helpers/Api-helper";
-import { Link } from "react-router-dom";
-
+import { Link, } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { adminActions, userActions } from "../../store";
 
 const Navbar = () => {
 
 const dummyArray = ["kings college", "MotherLand", "FatherLand"]
+
+// const navigate = useNavigate();
+const dispatch = useDispatch();
+const isAdminLoggedIn = useSelector((state) => state.admin.isLoggedIn);
+const isUserLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
+
+
 const [value, setValue] = useState(0);
 const [data, setData] = useState([]);
 
@@ -18,6 +26,10 @@ useEffect(() => {
       .catch((err) => console.log(err));
   }, []);
 //   console.log(data);
+
+const logout = (isAdmin) => {
+  dispatch(isAdmin ? adminActions.logout() : userActions.logout());
+};
 
   return (
     <AppBar position="sticky"  sx={{ bgcolor: "#2b2d42" }}>
@@ -50,8 +62,29 @@ useEffect(() => {
     <Box display="flex">
       <Tabs textColor="inherit" indicatorColor="secondary" value={value} onChange={(e, val) => setValue(val)} >
         <Tab LinkComponent={Link} to="/colleges" label="All Colleges"/>
-        <Tab LinkComponent={Link} to="/auth" label="Auth"/>
-        <Tab LinkComponent={Link} to="/admin" label="Admin"/>
+        {!isAdminLoggedIn && !isUserLoggedIn && (
+         <>
+          <Tab LinkComponent={Link} to="/auth" label="Auth"/>
+          <Tab LinkComponent={Link} to="/admin" label="Admin"/>
+         </>
+
+        )}
+          {isUserLoggedIn && (
+         <>
+          <Tab LinkComponent={Link} to="/user" label="Profile"/>
+          <Tab onClick={() => logout(false)} LinkComponent={Link} to="/" label="Logout"/>
+         </>
+
+        )}
+        {isAdminLoggedIn && (
+         <>
+          <Tab LinkComponent={Link} to="/addCollege" label="Add College"/>
+          <Tab LinkComponent={Link} to="/Profile" label="Profile"/>
+          <Tab onClick={() => logout(true)} LinkComponent={Link} to="/" label="Logout"/>
+         </>
+
+        )}
+        
       </Tabs>
     </Box>
   </Toolbar>
