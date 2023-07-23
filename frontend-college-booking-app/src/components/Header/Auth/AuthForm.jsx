063bb170c9ac83b285971bda741d21555abc5bd1
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-23 19:41:04 +0600

    Close button added to the auth form and redirrected to home page

--- frontend-college-booking-app/src/components/Header/Auth/AuthForm.jsx
+++ frontend-college-booking-app/src/components/Header/Auth/AuthForm.jsx
@@ -1,6 +1,7 @@
 import { Box, Button, Dialog, FormLabel, IconButton, TextField, Typography } from "@mui/material"
 import { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 const labelStyle = { mt: 1, mb: 1 };
 const AuthForm = () => {
     const [inputs, setInputs] = useState({
@@ -12,7 +13,7 @@ const AuthForm = () => {
     <Dialog PaperProps={{ style: { borderRadius: 20 } }} open={true}>
       <Box sx={{ ml: "auto", padding: 1 }}>
         <IconButton LinkComponent={Link} to="/">
-          {/* <CloseRoundedIcon /> */}
+          <CloseRoundedIcon />
         </IconButton>
       </Box>
       <Typography variant="h4" textAlign={"center"}>
@@ -69,17 +70,19 @@ const AuthForm = () => {
             fullWidth
             variant="contained"
           >
+            login
             {/* {isSignup ? "Signup" : "Login"} */}
           </Button>
-          {/* {!isAdmin && (
+          {/* {!isAdmin && ( */}
             <Button
-              onClick={() => setIsSignup(!isSignup)}
+            //   onClick={() => setIsSignup(!isSignup)}
               sx={{ mt: 2, borderRadius: 10 }}
               fullWidth
             >
-              Switch To {isSignup ? "Login" : "Signup"}
+              Switch To 
+              {/* {isSignup ? "Login" : "Signup"} */}
             </Button>
-          )} */}
+          {/* )} */}
         </Box>
       </form>
     </Dialog>
